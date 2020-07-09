ced3e598ada313c36180d726fd6186d925ccf7d0
itexpert120 <itexpert120@gmail.com>
2020-07-09 05:43:51 +0000

    Revert "msm8916-common: fix dependencies of CameraWrapper"
    This reverts commit 8d6a58debff66b54792f625b2879af750a829f1c.

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,15 +9,13 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_HEADER_LIBRARIES += \
-    libnativebase_headers \
-    libnativewindow_headers
+    libnativebase_headers
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils libcutils libdl \
     android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0 \
-    android.hardware.graphics.bufferqueue@2.0
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
