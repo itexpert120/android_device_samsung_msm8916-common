d1d84e694e4d7d2433bc13cccccde02939f85f1a
itexpert120 <itexpert120@gmail.com>
2020-07-16 14:29:46 +0000

    Uprev Power Hal to 1.2
    Change-Id: Ia4fbef5faa9d281df316476cb9fb16954fa31432

--- msm8916.mk
+++ msm8916.mk
@@ -371,8 +371,8 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
+    android.hardware.power@1.2-service-qti \
+    android.hardware.power.stats@1.0-service \
     power.qcom
 
 # Radio
