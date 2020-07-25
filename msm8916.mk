82eca386116c5de6a2ca1bb016284ff3b2ff98fe
itexpert120 <itexpert120@gmail.com>
2020-07-25 04:47:10 +0000

    Revert "add trust hal"
    This reverts commit 427addef01ef634c8935e88dcd089dd1306cd401.

--- msm8916.mk
+++ msm8916.mk
@@ -462,10 +462,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
