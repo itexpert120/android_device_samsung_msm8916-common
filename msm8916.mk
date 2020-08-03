cb62078c754820b88290d49fe17ef4df7b7b66c3
itexpert120 <itexpert120@gmail.com>
2020-08-03 09:39:35 +0000

    revert back to livedisplay 1.0
    Change-Id: I745c44c77ecb7415eb15890ee1f0755ae107c4a4

--- msm8916.mk
+++ msm8916.mk
@@ -179,8 +179,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay@1.0-service-legacymm
 
 # DRM
 PRODUCT_PACKAGES += \
