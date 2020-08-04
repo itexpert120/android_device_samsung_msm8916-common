4674505c0a75b724646c8c7d382088289e51f1f3
itexpert120 <itexpert120@gmail.com>
2020-08-04 06:23:30 +0000

    Introduce "SafailNet" Attempt to Bypass Safety Net
    Change-Id: Iaf0d33ffbeda3a028c0f5e7d2b39ee4b10b1331e

--- msm8916.mk
+++ msm8916.mk
@@ -417,7 +417,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     twrp.fstab \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
