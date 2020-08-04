d944a59dc50cebb60eb3a5d7ef70b1776ab0e5f3
itexpert120 <itexpert120@gmail.com>
2020-08-04 05:45:08 +0000

    Dont Add Pixel Fingerprint
    Change-Id: I62a6ef19dc1a72bfff9bae51d1baf802d7e9d6a6

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -188,5 +188,5 @@ void vendor_load_properties(void)
 	init_target_properties();
 
 	// fingerprint
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+        // property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 }
